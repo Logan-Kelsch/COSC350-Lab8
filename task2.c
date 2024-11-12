d90f6d0b755eab1f7c9c784ec063114196acaa7c
Logan-Kelsch <7logankelsch5@gmail.com>
2024-11-11 21:51:10 -0500

    task2 complete, 1,2 need comment+polish

--- task2.c
+++ task2.c
@@ -33,19 +33,66 @@ void err_sys(char *str){
 	exit(1);
 }
 
-//first thread function
+//first thread function\
+//moves all collected scores into global array
 void *t1_func(void *arg){
 	CLdata *data = (CLdata *)arg;
-	int scores[data->arg_c];
 	arr = (int*)malloc(data->arg_c*sizeof(int));
 	for(int i = 0; i < data->arg_c; i++)
 		arr[i] = atoi(data->arg_v[i]);
-	printf("END OF THREAD 1 REACHED. %d\n", arr[0]);
+	length = data->arg_c;
+	printf("Score data gathered.\n");
 	pthread_exit(NULL);
 }
 
 //second thread function
-void *t2_func(void *sum){
+//calculates average and median score
+void *t2_func(void *null){
+	int average = 0;
+	int median = 0;
+	//one line median calculation
+	median = (arr[(length-1)/2] + arr[length/2]) / 2;
+	//find and grab average value
+	for(int i = 0; i < length; i++)
+		average+=arr[i];
+	average/=length;
+	printf("Average of Scores: %d\n",average);
+	printf("Median of Scores: %d\n",median);
+	pthread_exit(NULL);
+}
+
+//third thread function
+//calculates min and max score
+void *t3_func(void *null){
+	int min, max;
+	min = arr[0];
+	max = arr[0];
+	//find and grab min and max values
+	for(int i = 1; i < length; i++){
+		if(arr[i]<min)
+			min=arr[i];
+		if(arr[i]>max)
+			max=arr[i];
+	}
+	printf("Maximum of Scores: %d\n",max);
+	printf("Minimum of Scores: %d\n",min);
+	pthread_exit(NULL);
+}
+
+//fourth thread function
+//clear array and call thread 2,3 functions??????
+void *t4_func(void *null){
+	for(int i = 0; i < length; i++)
+		arr[i] = 0;
+	printf("Buffer has been cleared.\n");
+
+	pthread_t t2_2, t3_2;
+
+	pthread_create(&t2_2, NULL, t2_func, NULL);
+	pthread_create(&t3_2, NULL, t3_func, NULL);
+	pthread_join(t2_2, NULL);
+	pthread_join(t3_2, NULL);
+
 	pthread_exit(NULL);
 }
 
@@ -62,15 +109,25 @@ int main(int argc, char *argv[]){
 
 	//thread declaration
 	pthread_t t1, t2, t3, t4;
-
-	pthread_create(&t1, NULL, t1_func, (void *)&cl);
+	
+	//thread 1 actions
+	pthread_create(&t1, NULL, t1_func, (void *)&cl);	
+	
 	pthread_join(t1, NULL);
 	
+	//thread 2-3 actions
 	pthread_create(&t2, NULL, t2_func, NULL);
-	pthread_join(t2, NULL);
-	
-	printf("MAIN COMPLETE. PROGRAM TERMINATING..\n");
+	pthread_create(&t3, NULL, t3_func, NULL);
 
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+	
+	//thread 4 actions
+	pthread_create(&t4, NULL, t4_func, NULL);
+
+	pthread_join(t4, NULL);
+	
+	//main program complete, terminate
 	pthread_exit(NULL);
 	exit(0);
 }
